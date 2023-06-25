890ec546d90f28e890a0c69c4af6f031f6e922be
Shanemacd10 <36234314+Shanemacd10@users.noreply.github.com>
2023-06-25 11:28:39 -0400

    Add files via upload

--- realestate_data_scrape.py
+++ realestate_data_scrape.py
@@ -9,71 +9,87 @@ import time
 import random
 
 def main():
-    #()--> (str) prompts user for url input and returns a string of unfiltered html house data
+    
+    def url_search(url):
+        #(str)-->(BeautifulSoup) takes a url string and outputs html soup object
+        html_txt = requests.get(url).text
+        soup = BeautifulSoup(html_txt, 'lxml')
+        return soup
+
+    def search_loop():
+        #()-->()iterates through price search params and runs page_loop to get data for each result (needed since royallepage.ca limits result to <=1000)
+        for x in range(1,10):
+            n = str(x*100000)
+            m = str(x*100000 + 99999)
+            soup = url_search(r'https://www.royallepage.ca/en/search/homes/?search_str=Nova+Scotia%2C+NS%2C+CAN&csrfmiddlewaretoken=gwr7nLTOgjCRzUfCho1OZ1vY4a1XWmfPkhXlSnpDItMAY1eaxcS4cXk3j3QhCZZS&property_type=&house_type=&features=&listing_type=&lat=45.76663995400003&lng=-61.63182999999998&upper_lat=&upper_lng=&lower_lat=&lower_lng=&bypass=&radius=&zoom=&display_type=gallery-view&travel_time=false&travel_time_min=30&travel_time_mode=drive&travel_time_congestion=&da_id=&segment_id=&tier2=False&tier2_proximity=0&address=Nova+Scotia&method=homes&address_type=province&city_name=&prov_code=NS&school_id=&min_price=' + n + r'&max_price=' + m +r'&min_leaseprice=0&max_leaseprice=5000%2B&beds=0&baths=0&transactionType=SALE&keyword=&sortby=')
+            temp_lst = soup.find('ul', class_='frow').text
+            if temp_lst is None:
+                pg_nums = 1
+            else:
+                pg_nums = int(temp_lst.split()[-1])
+                
+            page_loop(n, m, pg_nums, soup)
+    
+    def page_loop(n,m,pg_nums, soup):
+        #(str, str, int, BeautifulSoup)-->() iterates through each page of search with price search params and runs add_house_data to get data on each page
+        for x in range(1, pg_nums +1):
+            url = r'https://www.royallepage.ca/en/search/homes/' + str(x) + r'/?search_str=Nova+Scotia%2C+NS%2C+CAN&csrfmiddlewaretoken=gwr7nLTOgjCRzUfCho1OZ1vY4a1XWmfPkhXlSnpDItMAY1eaxcS4cXk3j3QhCZZS&property_type=&house_type=&features=&listing_type=&lat=45.76663995400003&lng=-61.63182999999998&upper_lat=&upper_lng=&lower_lat=&lower_lng=&bypass=&radius=&zoom=&display_type=gallery-view&travel_time=false&travel_time_min=30&travel_time_mode=drive&travel_time_congestion=&da_id=&segment_id=&tier2=False&tier2_proximity=0&address=Nova+Scotia&method=homes&address_type=province&city_name=&prov_code=NS&school_id=&min_price=' + n + r'&max_price=' + m + r'&min_leaseprice=0&max_leaseprice=5000%2B&beds=0&baths=0&transactionType=SALE&keyword=&sortby='
+            soup = url_search(url)
+            add_house_data(soup, house_list)
+            
+            #sleep function to not overwhelm royallepage website servers with requests
+            secs = random.randint(1, 10)
+            time.sleep(secs)
+            
+        page_loop('0', '50000', pg_nums, soup)
+        page_loop('50001', '99999', pg_nums, soup)
+        page_loop('1000000', '5000000%2B', pg_nums, soup)
+
     def add_house_data(soup, house_lst):
-        #(str, lst) --> () takes unfiltered html string and sorts and adds data points to a list
+        #(str, lst) --> () takes unfiltered html string and sorts and adds address, city, price, bedrooms, and bathrooms to house list
         houses = soup.find_all('li', class_ = 'card-group__item')
         for x in houses:
                 lst=[]
-            #Gets data from HTML code
+            #Gets adress, city, price, bedrooms, and bathrooms from HTML code 
                 address = x.find('address', class_ = 'address-1')
                 lst.append(none_type_check(address))
 
-                location = x.find('address', class_ = 'card__address-2')
-                lst.append(none_type_check(location))
+                city = x.find('address', class_ = 'card__address-2')
+                lst.append(none_type_check(city))
                 
                 price = x.find('span', class_ = 'title--h3 price')
                 lst.append(int(none_type_check(price)[1:].replace(',', '')))
                 
                 bed_bath = x.find('div', class_ = 'listing-meta listing-meta--small').text
-                
-            # Eliminates spaces in HTML code and adds the data points to a list
                 bed_bath = ' '.join(bed_bath.split())
                 bed_bath_nums = re.findall(r'\d+',bed_bath)
                 lst.append(bed_bath.split(' ',1)[0])
-                
-            # Gets number of bedrooms and bathrooms and appends to lst
                 bed_bath = re.findall(r'\d+',bed_bath)
                 for n in bed_bath:
                     lst.append(int(n))
                     
                 house_list.append(lst)
-    def none_type_check(house):
-            if house is None:
-                return None
-            else:
-                address = house.text
-                return ' '.join(address.split())
 
-    def page_loop():
-        for x in range(1, pg_nums +1):
-            url = 'https://www.royallepage.ca/en/search/homes/ns/halifax/' + str(x)+'/?property_type=&house_type=&features=&listing_type=&lat=44.648881000000074&lng=-63.57531199999994&bypass=&address=Halifax&address_type=city&city_name='+ city + r'&prov_code=' + prov + r'&display_type=gallery-view&da_id=&travel_time=&school_id=&search_str=Halifax%2C+NS%2C+CAN&id_search_str=Halifax%2C+NS%2C+CAN&school_search_str=&travel_time_min=30&travel_time_mode=drive&travel_time_congestion=&min_price=0&max_price=5000000%2B&min_leaseprice=0&max_leaseprice=5000%2B&beds=0&baths=0&transactionType=SALE&keyword='
-            soup = url_search(url)
-            add_house_data(soup, house_list)
-            secs = random.randint(1, 10)
-            time.sleep(secs)
-            
-    def url_search(url):
-        html_txt = requests.get(url).text
-        soup = BeautifulSoup(html_txt, 'lxml')
-        return soup
-        
-            
+    def none_type_check(house):
+        #(bs4.element.Tag)-->(str) takes a BeatifulSoup element and checks if it is none empty
+        if house is None:
+            return None
+        else:
+            temp = house.text
+            return ' '.join(temp.split())
+
     def add_sheet():
+        #writes data to Realestate_Data_Scrape.xlsx with sheet name RL_Data_day.month.year_Nova_Scotia
         now = datetime.now()
-        s_name = 'RL_Data_' + now.strftime('%d.%m.%Y_' + city +'_' + prov)
+        s_name = 'RL_Data_' + now.strftime('%d.%m.%Y_Nova_Scotia')
         df = pd.DataFrame(data = house_list, columns = ['Address', 'City', 'Price', 'Property_Type', 'Bedrooms', 'Bathrooms'])
-        writer = pd.ExcelWriter(r'C:\Users\Shane\OneDrive\Documents\Realestate_Data_Scrape.xlsx', engine='xlsxwriter')
+        writer = pd.ExcelWriter(r'C:\Users\Shane\Downloads\Excel\Realestate_Data_Scrape.xlsx', engine='xlsxwriter')
         df.to_excel(writer, sheet_name= s_name)
         writer._save()
-    city = input('Enter the city you would like to search: ')
-    prov = input ('Enter the province you would like to search (Abriviated): ')
+
     print('Please wait until data is downloaded')
-    temp = url_search(r'https://www.royallepage.ca/en/search/homes/ns/halifax/?property_type=&house_type=&features=&listing_type=&lat=44.648881000000074&lng=-63.57531199999994&bypass=&address=Halifax&address_type=city&city_name='+ city + '&prov_code=' + prov + '&display_type=gallery-view&da_id=&travel_time=&school_id=&search_str=Halifax%2C+NS%2C+CAN&id_search_str=Halifax%2C+NS%2C+CAN&school_search_str=&travel_time_min=30&travel_time_mode=drive&travel_time_congestion=&min_price=0&max_price=5000000%2B&min_leaseprice=0&max_leaseprice=5000%2B&beds=0&baths=0&transactionType=SALE&keyword=')
-    temp_lst = temp.find('ul', class_='frow').text
-    pg_nums = int(temp_lst.split()[-1])
     house_list = []
-    page_loop()
+    search_loop()
     add_sheet()
     print('Data Downloaded')
     
